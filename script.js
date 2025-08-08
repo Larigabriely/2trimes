a75a6fbc95ebe050eb42336695e85473e32b73fd
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-08-08 20:12:32 -0300

    00

--- script.js
+++ script.js
@@ -65,22 +65,7 @@ const perguntas = [
             }
             
         ]
-    },
-    {
-        enunciado: " Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: [
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao:"afirmacao"
-            },
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao:"afirmacao"
-            }
-            
-            
-        ]
-    },
+    }
 ];
 
 let atual = 0; 
